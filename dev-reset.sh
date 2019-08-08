fb2bc23265b5fc0ad69946ea26fdd9a94a238ec9
Saren Arterius <saren@wtako.net>
2019-08-08 13:11:08 +0800

    fix redis perm

--- dev-reset.sh
+++ dev-reset.sh
@@ -3,7 +3,6 @@ echo Resetting DB in 3 seconds...
 sleep 3
 docker-compose down
 sudo rm -rf redis/data postgres/data
-sudo mkdir -p redis/data
-sudo chown -R 100:101 redis/data
+sudo mkdir -p redis/data && sudo chown -R 999:1000 redis/data
 docker-compose up -d
 docker-compose logs --tail=1000 -f app
