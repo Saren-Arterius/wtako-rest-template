70da68e1d4a7cc9cddf0dc3d0b31eb9eb2d368b6
Saren Arterius <saren@wtako.net>
2019-08-08 18:45:30 +0800

    derp

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo mkdir -p redis/data && sudo chown -R 999:1000 redis/data
 docker-compose up -d
 docker-compose logs --tail=1000 -f app
--- reset.sh
+++ reset.sh
@@ -3,4 +3,3 @@ echo Resetting DB in 3 seconds...
 sleep 3
 docker-compose down
 sudo rm -rf redis/data postgres/data
-sudo mkdir -p redis/data && sudo chown -R 999:1000 redis/data
